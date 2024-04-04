e9eb69f1157079a90618d2e0ffea482606a75e3f
Nilesh Bhurewar <137190915+nileshbhurewar@users.noreply.github.com>
2024-04-04 15:32:45 +0530

    Create DuplicateCharacters.java

---
+++ DuplicateCharacters.java
@@ -0,0 +1,33 @@
+
+import java.util.Arrays;
+
+public class DuplicateCharacters {
+    
+    public static void findDuplicateCharacters(String str) {
+        
+        char[] characters = str.toCharArray();
+        
+        
+        Arrays.sort(characters);
+        
+        System.out.println("Duplicate characters in the string \"" + str + "\":");
+        
+        
+        for (int i = 0; i < characters.length - 1; i++) {
+            if (characters[i] == characters[i + 1]) {
+        
+                System.out.println(characters[i]);
+                
+                
+                while (i < characters.length - 1 && characters[i] == characters[i + 1]) {
+                    i++;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        String str = "This is a string to display duplicate character";
+        findDuplicateCharacters(str);
+    }
+}
