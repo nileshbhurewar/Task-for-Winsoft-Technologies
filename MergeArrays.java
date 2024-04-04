db0223720d05ad4cc6837368fbd526bebefa75b1
Nilesh Bhurewar <137190915+nileshbhurewar@users.noreply.github.com>
2024-04-04 15:34:30 +0530

    Create MergeArrays.java

---
+++ MergeArrays.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class MergeArrays {
+    
+    public static void mergeArrays(int[] X, int[] Y) {
+        int m = X.length;
+        int n = Y.length;
+        
+        int k = 0; 
+        
+        
+        for (int i = 0; i < m; i++) {
+            if (X[i] == 0 && k < n) {
+                X[i] = Y[k++]; 
+            }
+        }
+        
+        
+        Arrays.sort(X);
+    }
+    
+    public static void main(String[] args) {
+        int[] X = {0, 2, 0, 3, 0, 5, 6, 0, 0};
+        int[] Y = {1, 8, 9, 10, 15};
+        
+        
+        mergeArrays(X, Y);
+        
+       
+        System.out.print("Merged Array X[] = { ");
+        for (int num : X) {
+            System.out.print(num + " ");
+        }
+        System.out.println("}");
+    }
+}
